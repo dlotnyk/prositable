402550bdfc89695e27e23b18294f1110a28cb499
dmytro.lotnyk@gmail.com <dmytro.lotnyk@gmail.com>
2022-07-31 00:27:29 +0200

    add delete_entry
    Signed-off-by: dmytro.lotnyk@gmail.com <dmytro.lotnyk@gmail.com>

--- src/tables.py
+++ src/tables.py
@@ -1,8 +1,8 @@
+from sqlalchemy.exc import IntegrityError
 from typing import Optional, List, Tuple
 from basic_defs import KnownFrom, Cities
 from local_db import LocalDb
-from datetime import datetime
-from datetime import date
+from datetime import datetime, date
 from table_schemas import MainTable, main_table_name, Base
 from logger import log_settings
 app_log = log_settings()
@@ -52,7 +52,7 @@ class OperateMainTable:
         else:
             return today.year - born.year
 
-    def _get_birth(self, dbirth: str) -> Tuple[Optional[datetime], Optional[int]]:
+    def _get_birth(self, dbirth: str) -> Tuple[Optional[date], Optional[int]]:
         if dbirth:
             born = datetime.strptime(dbirth, "%Y-%m-%d")
             age = self._calculate_age(born)
@@ -78,16 +78,38 @@ class OperateMainTable:
                              city=city)
             self._open()
             if self._dbase and self._dbase.session:
-                self._dbase.session.add(data)
-                self._dbase.session.commit()
-                app_log.debug(f"Entry inserted to `{MainTable.__tablename__}`")
+                try:
+                    self._dbase.session.add(data)
+                    self._dbase.session.commit()
+                    app_log.debug(f"Entry inserted to `{MainTable.__tablename__}`")
+                except IntegrityError:
+                    app_log.error(f"Can not insert into {MainTable.__tablename__}: id already exists")
+                except Exception as ex1:
+                    app_log.error(f"Can not insert into {MainTable.__tablename__}: {ex1}")
+                finally:
+                    self._close()
         except Exception as ex:
             app_log.error(f"Can not insert into {MainTable.__tablename__}: {ex}")
 
+    def delete_entry(self, client_id: int) -> None:
+        try:
+            self._open()
+            obj = self._dbase.session.query(MainTable).filter(MainTable.client_id == client_id).first()
+            self._dbase.session.delete(obj)
+            self._dbase.session.commit()
+            app_log.debug(f"Entry `{client_id}` deleted from `{MainTable.__tablename__}`")
+        except Exception as ex:
+            app_log.error(f"Can not deleted from {MainTable.__tablename__}: {ex}")
+        finally:
+            self._close()
+
 
 if __name__ == "__main__":
-    OperateMainTable().insert_entry(client_id=2, name="test_name", surname="test_surname",
-                                    age=11,
+    OperateMainTable().insert_entry(client_id=0, name="test_name", surname="test_surname",
+                                    birth="2008-08-11",
                                     known_from=KnownFrom.university, city=Cities.Kosice)
     resp = OperateMainTable().select_all()
-    print(resp[0].surname)
+    OperateMainTable().delete_entry(0)
+    for item in resp:
+        print(f"{item.client_id} - {item.name} - {item.surname} - {item.known_from} - {item.birth} - {item.age} - "
+              f"{item.phone} - {item.city}")
