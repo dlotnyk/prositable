f30b5fdadac6aeaff440afb65609c7912a9d7d26
dlvision <dmytro.lotnyk@gmail.com>
2022-08-18 19:07:16 +0200

    update logger
    Signed-off-by: dlvision <dmytro.lotnyk@gmail.com>

--- src/logger.py
+++ src/logger.py
@@ -1,11 +1,12 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from dbs.db_defs import log_path
 
 
 def log_settings():
     #  Logger definitions
     log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(funcName)s - line: %(lineno)d - %(message)s')
-    logFile = "prositable.log"
+    logFile = log_path
     my_handler = RotatingFileHandler(logFile, mode="a", maxBytes=20*1024*1024, backupCount=2, encoding=None, delay=False)
     my_handler.setFormatter(log_formatter)
     my_handler.setLevel(logging.INFO)
